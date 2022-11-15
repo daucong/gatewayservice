7c2f548da728016cb141affd6d9bc460494668fe
Admin <dauthaicong11092001@gmail.com>
2022-11-15 16:53:42 +0700

    fix CROS

---
+++ src/main/java/com/example/gatewayservice/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.gatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfiguration extends
+        org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+}
